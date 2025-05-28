c345ae4a2db4c2b641ec5546ea80e93e320fc611
Yash Lad <36941068+ifis98@users.noreply.github.com>
2025-05-28 19:23:37 -0400

    Fix gear icon and splash screens

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  assets: ['./src/assets/fonts'],
+  assets: ['./src/assets/fonts', './node_modules/react-native-vector-icons/Fonts'],
   project: {
     android: {
-      packageName: "com.bruxaway",
+      packageName: 'com.bruxaway',
     },
   },
 };
